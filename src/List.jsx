e7ac5b6935e578702b478e3b9d188abedc42721e
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2022-12-02 23:48:25 +0100

    redux connected

--- src/List.jsx
+++ src/List.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const List = ({rates}) => {
-  const ratesElements = rates.map(rate => (<Element key={rate.id} {...rate}/>
-    ));
-  return <ul>
-    {ratesElements}
-  </ul>;
+const List = ({ ratesInProps }) => {
+  const ratesElements = ratesInProps.map((rate) => (
+    <Element key={rate.id} {...rate} />
+  ));
+  return <ul>{ratesElements}</ul>;
 };
 
-export default List;
+const connectReduxStateToProps = (store) => ({
+  ratesInProps: store.rates,
+});
+
+const ListConsumer = connect(connectReduxStateToProps)(List);
+
+export default ListConsumer;
