b5a63f4f4e7398d07b65fae8b8e330fc3532f2f9
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2022-12-07 09:29:48 +0100

    add useSelector

--- src/List.jsx
+++ src/List.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import Element from './Element';
 
-const List = ({ ratesInProps }) => {
-  const ratesElements = ratesInProps.map((rate) => (
+
+const List = () => {
+  const rates = useSelector((store) => store.rates);
+  const ratesElements = rates.map((rate) => (
     <Element key={rate.id} {...rate} />
   ));
   return <ul>{ratesElements}</ul>;
 };
 
-const connectReduxStateToProps = (store) => ({
-  ratesInProps: store.rates,
-});
-
-const ListConsumer = connect(connectReduxStateToProps)(List);
-
-export default ListConsumer;
+export default List;
