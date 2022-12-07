277cec10777cfb03e58dad4ea45945dfb5aa6e99
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2022-12-07 09:03:43 +0100

    finish validateAuthorMiddleware

--- src/middleware/validateAuthorMiddleware.js
+++ src/middleware/validateAuthorMiddleware.js
@@ -1,7 +1,14 @@
 import { ADD } from '../actions/appActions';
 
-export const validateAuthorMiddleware = store => next => action => {
-  console.log('Jesteśmy w middlewarze :) ');
+export const validateAuthorMiddleware =
+  ({ dispath }) =>
+  (next) =>
+  ({ payload, type }) => {
+    console.log('Jesteśmy w middlewarze :) ');
+    if (type === ADD && !payload.author.lenght) {
+      console.warn('Autor nie ma imienia i nazwiska');
+      return;
+    }
 
-  next(action);
-}
+    next(action);
+  };
